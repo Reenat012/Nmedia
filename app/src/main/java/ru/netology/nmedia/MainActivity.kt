a80f37229f37089f47154be6cd28b25b32bcdc29
Reenat <reenat056@yandex.ru>
2024-03-26 20:36:20 +0300

    fix fun like

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import ru.netology.nmedia.R.drawable.like_svgrepo_com
+import ru.netology.nmedia.R.drawable.like_svgrepo_com__1_
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,30 +20,36 @@ class MainActivity : AppCompatActivity() {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
-            likedByMe = false
+            likedByMe = false,
         )
-        with(binding){
+        with(binding) {
             tvAuthor.text = post.author
             tvPublished.text = post.published
-            tvContent.text= post.content
+            tvContent.text = post.content
+            tvRepost.text = post.countReposts.toString()
             if (post.likedByMe) {
-                ivLikes?.setImageResource(R.drawable.like_svgrepo_com__1_)
+                ivLikes?.setImageResource(like_svgrepo_com__1_)
             }
 
             var counterLikes = 0
 
-            ivLikes.setOnClickListener{
-                post.likedByMe = !post.likedByMe //меняем false на true
+            ivLikes?.setOnClickListener {
+                post.likedByMe = !post.likedByMe
                 ivLikes.setImageResource(
-                    if (post.likedByMe) {
-                        R.drawable.like_svgrepo_com__1_
-                        counterLikes++
-                    } else {
-                        R.drawable.like_svgrepo_com
-                        counterLikes--
-                    }
-                )
+                    if (post.likedByMe) like_svgrepo_com__1_ else like_svgrepo_com)
+                if (post.likedByMe) {
+                    counterLikes++
+                } else {
+                    if (counterLikes > 0) counterLikes--
+                }
                 tvLikes.text = counterLikes.toString()
+            }
+
+            var countReposts = 0
+
+            ivRepost.setOnClickListener {
+                countReposts++
+                tvRepost.text = countReposts.toString()
             }
         }
     }
@@ -52,5 +60,6 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likedByMe: Boolean = false
+    var likedByMe: Boolean = false,
+    var countReposts: Int = 0
 )
